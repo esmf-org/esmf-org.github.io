fe3a0b746e57573933de7c6d883283136ed29f6e
Himanshu Pillai <himanshu@bighorn.cgd.ucar.edu>
2020-06-24 13:23:21 -0600

    pushing nuopc doc Wed Jun 24 13:23:21 MDT 2020

--- dev_docs/NUOPC_refdoc/images.pl
+++ dev_docs/NUOPC_refdoc/images.pl
@@ -1,16 +1,40 @@
-# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# LaTeX2HTML 2012 (1.2)
 # Associate images original text with physical files.
 
 
+$key = q/>>>;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="575" HEIGHT="182" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$&#187;&gt;$">|; 
+
+$key = q/<<<;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="41" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$&#171;&lt;$">|; 
+
+$key = q/scalebox{0.6}{includegraphics{NUOPC_GC}};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="65" HEIGHT="34" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\scalebox{0.6}{\includegraphics{NUOPC_GC}}">|; 
+
+$key = q/scalebox{0.6}{includegraphics{NUOPC_RunSequence}};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="46" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\scalebox{0.6}{\includegraphics{NUOPC_RunSequence}}">|; 
+
 $key = q/1000s;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="43" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ WIDTH="35" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img6.png"
  ALT="$1000s$">|; 
 
 $key = q/100s;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="35" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ WIDTH="41" HEIGHT="29" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img4.png"
  ALT="$100s$">|; 
 
@@ -25,30 +49,6 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="35" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img5.png"
  ALT="$800s$">|; 
-
-$key = q/<<<;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="41" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$&#171;&lt;$">|; 
-
-$key = q/>>>;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="41" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$&#187;&gt;$">|; 
-
-$key = q/scalebox{0.6}{includegraphics{NUOPC_GC}};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="575" HEIGHT="182" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="\scalebox{0.6}{\includegraphics{NUOPC_GC}}">|; 
-
-$key = q/scalebox{0.6}{includegraphics{NUOPC_RunSequence}};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="721" HEIGHT="495" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img8.png"
- ALT="\scalebox{0.6}{\includegraphics{NUOPC_RunSequence}}">|; 
 
 1;
 
